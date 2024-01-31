3d00f0e307fdc8ed0526ef2ccd4b3c051f79593a
Oscar Schott <oscar.schott@gmail.com>
2024-01-30 21:57:31 -0800

    added basic feedbackMode (untested)

--- exoControl.ino
+++ exoControl.ino
@@ -25,7 +25,7 @@ float gearboxRatio = 90.0;
 float eRPM = 3000.0;
 float RPM = eRPM/7.0; //240.0;
 bool isRunning = false;
-//bool feedbackMode = false; //New feature
+bool feedbackMode = false; //New feature
 
 // Device/Port initializations
 void setup() {
@@ -42,16 +42,23 @@ void setup() {
 void loop() { // Super-Loop for CPM functionality
 
   // Setting forward/reverse duty cycle using FSR values
-  //pressureFeedback = (analogRead(FSRpin1)/1023.0)-(analogRead(FSRpin2)/1023.0);
+  pressureFeedback = (analogRead(FSRpin1)/1023.0)-(analogRead(FSRpin2)/1023.0);
+  if (feedbackMode) {
+    if (pressureFeedback > 0.02 || pressureFeedback < -0.02) {
+        if (pressureFeedback < 0.1 && pressureFeedback > -0.1) {
+            vesc.setDuty(pressureFeedback);
+        }
+    }
+  }
 
   // Running motor
-  if (abs(pressureFeedback) > resistanceLevel && abs(pressureFeedback) < 0.1) {
+  /*if (abs(pressureFeedback) > resistanceLevel && abs(pressureFeedback) < 0.1) {
     vesc.setDuty(0.0); // stop motor
     //vesc.setDuty(pressureFeedback);
     isRunning = false;
     Serial.println("stopped motor");
   // Normal function  
-  } else if (isRunning) vesc.setRPM(eRPM); 
+  } else*/ if (isRunning) vesc.setRPM(eRPM);
   
   
   // Increment count if 125ms has passed
@@ -63,8 +70,10 @@ void loop() { // Super-Loop for CPM functionality
 
   // Change direction of motor and restart count
   if (romCount >= romTime/timeIncrement) {
-    vesc.setDuty(0.0);
-    delay(1000);
+    if (!feedbackMode) {
+      vesc.setDuty(0.0);
+      delay(1000);
+    }
 
     eRPM = eRPM * (-1);
     romCount = 0;
@@ -86,14 +95,16 @@ void loop() { // Super-Loop for CPM functionality
   // New instruction from app
   if (SerialBluetooth.available()) {
     
-    char message[8];
-    for (int i = 0; i < 8; i++) message[i] = SerialBluetooth.read();
+    // Put new message in char array
+    char message[4];
+    for (int i = 0; i < 4; i++) message[i] = SerialBluetooth.read();
     Serial.println(message);
     
     // Handle message[] instruction
     switch (message[0]) {
       
       case '1': // Start/Stop
+        feedbackMode = false;
         if (message[1] == '1') isRunning = true;
         else isRunning = false;
         Serial.print("Motor status: ");
@@ -106,7 +117,6 @@ void loop() { // Super-Loop for CPM functionality
                   + atoi(message[3]);
         romTime = (((romDegrees * gearboxRatio) / 360) 
                   / (RPM / 60)) * 1000; //ms
-        //(romDegrees / ((RPM / 60) * 4)) * 1000; // (Theta / ()
         Serial.print("NEW ROM: ");
         Serial.println(romDegrees, DEC);
         Serial.print("Movement duration (ms): ");
@@ -136,7 +146,9 @@ void loop() { // Super-Loop for CPM functionality
 
       //This setting allows the user to move the arm based on FSR data
       case '6': 
-        
+        isRunning = false;
+        feedbackMode = true;
+        Serial.println("FEEDBACK MODE ENABLED");
       break;
 
       default: break;
